2fb8802f58dab53dbdf83e3f8058c3142733b882
sfusco <sfusco@ricin.local>
2013-04-30 11:28:49 -0400

    adding npm update to system update

--- updatesystem.sh
+++ updatesystem.sh
@@ -11,3 +11,5 @@ gem cleanup -q
 # uninstall
 #gem list --no-versions | xargs sudo gem uninstall -aIx -i /System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/lib/ruby/gems/1.8
 #gem list --no-versions | xargs sudo gem uninstall -aIx -i /Library/Ruby/Gems/1.8 
+
+npm update -g
