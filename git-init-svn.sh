d9a2e0a8f656d35ea521bd5d2db7851ae7e83c1a
sfusco <sfusco@ricin.local>
2014-01-16 11:41:12 -0500

    wrapping string param in quotes

--- git-init-svn.sh
+++ git-init-svn.sh
@@ -4,9 +4,9 @@ svn_parent_dir=$1
 local_dir=`echo $svn_parent_dir | sed 's/.*\///'`
 commit_msg='init new project folders'
 
-svn mkdir $svn_parent_dir -m $commit_msg
-svn mkdir $svn_parent_dir/trunk -m $commit_msg
-svn mkdir $svn_parent_dir/tags -m $commit_msg
-svn mkdir $svn_parent_dir/branches -m $commit_msg
+svn mkdir $svn_parent_dir -m "$commit_msg"
+svn mkdir $svn_parent_dir/trunk -m "$commit_msg"
+svn mkdir $svn_parent_dir/tags -m "$commit_msg"
+svn mkdir $svn_parent_dir/branches -m "$commit_msg"
 
 git svn clone $svn_parent_dir/trunk $local_dir
