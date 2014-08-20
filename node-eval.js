bca50036f07a51a89533e99c49c3eacabbbb80aa
sfusco <sfusco@ricin.local>
2014-08-19 21:21:40 -0400

    error handling

--- node-eval.js
+++ node-eval.js
@@ -1,10 +1,20 @@
 #!/usr/bin/env node
 
-process.stdin.on('data', function (d) {
-  process.stdout.write('' + map(d));
-});
+try {
 
-function map (d) {
-  o = eval(process.argv[2]);
-  return o;
+  process.stdin.on('data', function (d) {
+    process.stdout.write('' + map(d));
+  });
+
+  process.on('uncaughtException', function () {
+    process.exit(1);
+  });
+
+  function map (d) {
+    o = eval(process.argv[2]);
+    return o;
+  }
+
+} catch (ex) {
+  process.exit(2);
 }
